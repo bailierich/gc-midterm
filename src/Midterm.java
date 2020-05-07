f5fcefceca9aea66bd5a0e2c04ae8a7b81ba50c3
dylan <dylanwalshprograms@gmail.com>
2020-05-07 10:19:22 -0400

    added third line.

--- src/Midterm.java
+++ src/Midterm.java
@@ -4,6 +4,7 @@ public class Midterm {
 	public static void main(String[] args) {
 		System.out.println("Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.");
 		System.out.println("On a cold dark day, the evil Shiera attacked Shangrilla. ");
+		System.out.println("Kai yun and his ninja monkeys were the only hope for Shangrilla.");
 	
 	}
 
