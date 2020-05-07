22c6287100af3f09b29dd9af2d46f6b07a95d118
dylan <dylanwalshprograms@gmail.com>
2020-05-07 11:17:10 -0400

    changed david and students

--- src/Midterm.java
+++ src/Midterm.java
@@ -2,11 +2,10 @@
 public class Midterm {
 
 	public static void main(String[] args) {
-		System.out.println("Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.");
+		System.out.println("Once upon a time in the mystical land of Shangrila there dwelt an immortal named David and his trusty band of ninja students.");
 		System.out.println("On a cold dark day, the evil Shiera attacked Shangrilla. ");
-		System.out.println("Kai-Yun and his ninja monkeys were the only hope for Shangrilla.");
-		System.out.println("Kai-Yun wasn't prepared for the sheer force of Shiera powers, Shangrilla was destroyed.");
-		System.out.println("Kai-Yun rose from the ashes in a daze, he called out to his ninja monkeys, but heard no response.");
+		System.out.println("David and his ninja students were the only hope for Shangrilla.");
+		System.out.println("David wasn't prepared for the sheer force of Shiera powers, Shangrilla was destroyed.");
+		System.out.println("David rose from the ashes in a daze, he called out to his ninja students, but heard no response.");
 	}
-
 }
