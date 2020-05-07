dace44de79c736e707d49f1683cf46f379f4c653
bailierich <bailie.richard@gmail.com>
2020-05-07 13:05:22 -0400

    restart

--- src/Midterm.java
+++ src/Midterm.java
@@ -3,11 +3,6 @@ public class Midterm {
 
 	public static void main(String[] args) {
 
-		System.out.println("Once upon a time in the mystical land of Detroit there dwelt an immortal named David and his trusty band of ninja students.");
-		System.out.println("On a cold dark day, the evil Shiera attacked Detroit. ");
-		System.out.println("David and his ninja students were the only hope for Shangrilla.");
-		System.out.println("David wasn't prepared for the sheer force of Shiera powers, Shangrilla was destroyed.");
-		System.out.println("David rose from the ashes in a daze, he called out to his ninja students, but heard no response.");
-		System.out.println("Those traitorous monkeys had switched sides and were now fighting against him with Shiera!");
+		
 	}
 }
