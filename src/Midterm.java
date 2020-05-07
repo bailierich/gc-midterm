76a8f51b510b8b749298ba5ddec9129a2c823ad6
bailierich <bailie.richard@gmail.com>
2020-05-07 11:19:15 -0400

    change

--- src/Midterm.java
+++ src/Midterm.java
@@ -2,10 +2,10 @@
 public class Midterm {
 
 	public static void main(String[] args) {
-		System.out.println("Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.");
-		System.out.println("On a cold dark day, the evil Shiera attacked Shangrilla. ");
-		System.out.println("Kai-Yun and his ninja monkeys were the only hope for Shangrilla.");
-		System.out.println("Kai-Yun wasn't prepared for the sheer force of Shiera powers, Shangrilla was destroyed.");
+		System.out.println("Once upon a time in the mystical land of Detroit there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.");
+		System.out.println("On a cold dark day, the evil Shiera attacked Detroit. ");
+		System.out.println("Kai-Yun and his ninja monkeys were the only hope for Detroit.");
+		System.out.println("Kai-Yun wasn't prepared for the sheer force of Shiera powers, Detroit was destroyed.");
 		System.out.println("Kai-Yun rose from the ashes in a daze, he called out to his ninja monkeys, but heard no response.");
 		System.out.println("Those traitorous monkeys had switched sides and were now fighting against him with Shiera!");
 	}
